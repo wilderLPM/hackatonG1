870234c425bd72987cce63f95d7f94de3d86ad3a
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-16 17:13:07 +0200

    Adding Cors-example

--- NODEJS/src/config/index.ts
+++ NODEJS/src/config/index.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const CONFIG_APP = {
-    PORT_SERVER: process.env.SERVER_PORT
+    PORT_SERVER: process.env.SERVER_PORT,
+    ORIGIN: process.env.ORIGIN
 }
 
 export default CONFIG_APP;
