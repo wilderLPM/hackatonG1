5076467a80669083eec411dab20f06024e8582ee
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-16 15:23:28 +0200

    add loading effect

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,16 +1,55 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../ui_components/Loader.css";
 import styles from "./Map.module.css";
+import TreeZero from "../assets/treezero.svg";
+import TreeOne from "../assets/treeone.svg";
+import TreeTwo from "../assets/treetwo.svg";
+import TreeThree from "../assets/treethree.svg";
 
 export default function Map() {
   const [isLoading, setIsLoading] = useState(true);
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [TreeZero, TreeOne, TreeTwo, TreeThree];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 1000);
+    return () => clearInterval(intervalId); // Clean up the interval on component unmount
+  }, [images.length]); // Re-run effect if the number of images changes
+
   const { destination } = useParams();
+
   return (
     <>
       <div className={styles.container}>
-        {isLoading === true && <div className="loader" />}
+        {isLoading === true &&
+        destination !== "Montagne" &&
+        destination !== "Forêt" ? (
+          <div className={destination} />
+        ) : destination === "Montagne" ? (
+          <section className="circle">
+            <div className="lake">
+              <div></div>
+            </div>
+            <div className="clouds">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="mountain">
+              <div></div>
+            </div>
+            <div className="mountain">
+              <div></div>
+            </div>
+          </section>
+        ) : (
+          <img src={images[currentIndex]} alt="tree" />
+        )}
       </div>
       <p>{destination}</p>
     </>
