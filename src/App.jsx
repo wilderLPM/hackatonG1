a6be0befc26773431eb358f8399f369a78e3a0c8
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-16 11:29:59 +0200

    create route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
