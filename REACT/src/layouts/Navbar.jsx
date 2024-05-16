c4f322e12c07a534c9de179e8c37aec3216a82ee
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-16 23:08:54 +0200

    fixed error

--- REACT/src/layouts/Navbar.jsx
+++ REACT/src/layouts/Navbar.jsx
@@ -1,5 +1,9 @@
-export default function Navbar(){
+import styles from "./NavBar.module.css";
+
+export default function NavBar(){
     return(
-        <></>
+        <div className={styles.navBar} >
+            <h1>The Great Wild White</h1>
+        </div>
     );
 }
