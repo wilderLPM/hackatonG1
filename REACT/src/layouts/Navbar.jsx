7ed4fa93c603cd0575da925ac166fa25e47e17e6
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-17 12:42:50 +0200

    add link on the title

--- REACT/src/layouts/Navbar.jsx
+++ REACT/src/layouts/Navbar.jsx
@@ -1,9 +1,12 @@
 import styles from "./NavBar.module.css";
+import {Link} from "react-router-dom";
 
 export default function NavBar(){
     return(
         <div className={styles.navBar} >
+            <Link to="/"> 
             <h1>The Great Wild <p>White</p></h1>
+            </Link>
         </div>
     );
 }
