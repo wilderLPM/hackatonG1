cef43f0b2fa892d3de65c3b3041eb5553ab91740
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-17 09:43:12 +0200

    Finish polygons roads for python

--- NODEJS/src/routes/FrontRoads.ts
+++ NODEJS/src/routes/FrontRoads.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { index } from "../controllers/FrontController.js";
+import { index, find } from "../controllers/FrontController.js";
 
 const FrontRoads = Router();
 
-// Say if lat and long are inside multiple polygon
-FrontRoads.get('/polygon/:lat/:long', index);
 
-// Roads for get python datas
+// Roads for get polygons datas
+FrontRoads.get('/polygons', index)
+
+// Say if lat and long are inside multiple polygon
+FrontRoads.get('/polygon/:lat/:long', find);
+
+// Roads for get points datas
 
 export default FrontRoads;
