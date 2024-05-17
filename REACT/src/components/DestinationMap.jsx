329607d3875dd20deb55dc82fa2683915f3e6ce2
Léo-Paul Montalbetti <montalbetti.lp@gmail.com>
2024-05-17 12:26:34 +0200

    second page now displays a map

--- REACT/src/components/DestinationMap.jsx
+++ REACT/src/components/DestinationMap.jsx
@@ -1,22 +1,10 @@
-import {useState, useEffect} from 'react';
+import data from '../map.html';
 
 export default function DestinationMap({setIsLoading}){
-    const [htmlContent, setHtmlContent] = useState('');
-    useEffect(() => {
-        console.log('fetching...');
-        fetch('../map.html')
-        .then(response => response.text())
-        .then(data => {
-            setHtmlContent(data);
-            setIsLoading(false);
-        })
-        .catch(error => console.error('Error fetching HTML:', error));
-    }, []);
     
     return (
         <div>
-        <h1>Loaded HTML Content</h1>
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        <iframe src={data} onLoad={() => setIsLoading(false)} ></iframe>
         </div>
     );
 }
--- REACT/src/ui_components/Loader.jsx
+++ REACT/src/ui_components/Loader.jsx
@@ -4,30 +4,30 @@ export default function Loader({isLoading, destination, currentIndex, images}){
 
     return(
         <div className="loadersContainer">
-        {isLoading === true &&
-        destination !== "Chamonix" &&
-        destination !== "Grenoble" ? (
-          <div className={destination} />
-        ) : destination === "Chamonix" ? (
-          <section className="circle">
-            <div className="lake">
-              <div></div>
-            </div>
-            <div className="clouds">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <div className="mountain">
-              <div></div>
-            </div>
-            <div className="mountain">
-              <div></div>
-            </div>
-          </section>
-        ) : (
-          <img src={images[currentIndex]} alt="tree" />
-        )}
+          {isLoading === true ?
+          (destination !== "Chamonix" &&
+          destination !== "Grenoble" ? (
+            <div className={destination} />
+          ) : destination === "Chamonix" ? (
+            <section className="circle">
+              <div className="lake">
+                <div></div>
+              </div>
+              <div className="clouds">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <div className="mountain">
+                <div></div>
+              </div>
+              <div className="mountain">
+                <div></div>
+              </div>
+            </section>
+          ) : (
+            <img src={images[currentIndex]} alt="tree" />
+          )) : null}
       </div>
     );
 }
